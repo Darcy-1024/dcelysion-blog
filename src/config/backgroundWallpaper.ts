138b5f576c9491d3c2c1aed1e76e17a006aab0c0
Darcy <Lin507793465@gmail.com>
2026-07-09 03:25:30 +0800

    配置v0.1

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -43,12 +43,6 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
 			"assets/images/DesktopWallpaper/d7.avif",
 			"assets/images/DesktopWallpaper/d8.avif",
 			"assets/images/DesktopWallpaper/d9.avif",
@@ -71,12 +65,6 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/MobileWallpaper/m1.avif",
-			"assets/images/MobileWallpaper/m2.avif",
-			"assets/images/MobileWallpaper/m3.avif",
-			"assets/images/MobileWallpaper/m4.avif",
-			"assets/images/MobileWallpaper/m5.avif",
-			"assets/images/MobileWallpaper/m6.avif",
 			"assets/images/MobileWallpaper/m7.avif",
 			"assets/images/MobileWallpaper/m8.avif",
 			"assets/images/MobileWallpaper/m9.avif",
