00d1c3275ecc74634ebd0a5a52b4b4e519a3c4d9
Darcy <Lin507793465@gmail.com>
2026-09-08 20:03:31 +0800

    feat: 更新站点 favicon 图标

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -89,7 +89,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/avatar-32.png",
+			src: "/favicon/avatar-cutout-64.png",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
